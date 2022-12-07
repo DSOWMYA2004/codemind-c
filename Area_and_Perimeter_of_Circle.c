7160e3349c9826362b2cc154d78233c08e49b47d
DSOWMYA2004 <119849225+DSOWMYA2004@users.noreply.github.com>
2022-12-07 06:04:57 -0800

    #include<stdio.h> int main() {     int r;     scanf("%d",&r);     float a;     a=3.14*r*r;     float x;     x=2*3.14*r;     printf("%0.2f ",a);     printf("%0.2f ",x); }

---
+++ Area_and_Perimeter_of_Circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    float a;
+    a=3.14*r*r;
+    float x;
+    x=2*3.14*r;
+    printf("%0.2f
+",a);
+    printf("%0.2f
+",x);
+}
