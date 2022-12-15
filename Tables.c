badacca1c861edd658a2e443e68789073fe5bb5b
DSOWMYA2004 <119849225+DSOWMYA2004@users.noreply.github.com>
2022-12-15 14:52:31 +0530

    #include<stdio.h> int main() {     int a,b,i,t;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2==1){     t=a*i;     printf("%d x %d = %d ",a,i,t); } } }

---
+++ Tables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,t;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2==1){
+    t=a*i;
+    printf("%d x %d = %d
+",a,i,t);
+}
+}
+}
